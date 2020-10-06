7087ebc4137c0db6dbdcc5ec3219b61c1f207827
Miguel RA <miguel.ra@mobgen.com>
2020-10-06 21:33:46 +0200

    Removed initial components and create first layout approach

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>Browse</li>
+            <li>Add New Questions</li>
+            <li>API</li>
+            <li>Discuss</li>
+            <li>Login</li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        <section>
+          <h1>Browse Questions</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Type</th>
+                <th>Difficulty</th>
+                <th>Question</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Category</td>
+                <td>Type</td>
+                <td>Difficulty</td>
+                <td>Question</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </main>
+    </>
   );
 }
 
